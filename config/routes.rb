f5b79b8b015c91938fc33158d72632d1994c42b4
Colin Tseng <klt.aus@yahoo.com>
2021-09-04 18:23:21 +1000

    Completed assignment

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,10 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :students, only: [:index, :new, :create]
   get '/student/:id', to: 'students#show', as: 'student'
+  get '/students', to: 'students#index'
+  get '/students/new', to: 'students#new'
+  post '/students/new', to: 'students#create'
+
+
+
 end
